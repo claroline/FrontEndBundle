73a60b73c8abe71d72be63884f275b2fe7f309be
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-02 17:14:51 +0200

    Prepared kernel bundle integration

--- FrontEndBundle.php
+++ FrontEndBundle.php
@@ -3,7 +3,18 @@
 namespace Claroline\Bundle\FrontEndBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class FrontEndBundle extends Bundle
+class FrontEndBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environment)
+    {
+        return true;
+    }
+
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
